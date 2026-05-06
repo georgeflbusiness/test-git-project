b7705ef7525ad99575c3bd1ff82ea7a19f94e651
CodeByJeo <georgefl.business@gmail.com>
2026-05-06 22:45:12 +0300

    adding a new immutable string

--- src/gr/aueb/cf/schoolapp/Main.java
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         String[] test = {"test","test2","τεστ3"};
         System.out.println(test[0]);
-        System.out.println("test");
+        System.out.println("giorgos_floros euxaristw poyl na eiste kalak ai eseiw kai egw omvw oxi moino seisew eyxaeiata");
         printArray(test);
 
     }
