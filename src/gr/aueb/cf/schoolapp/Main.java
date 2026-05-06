82fe90d814014db28ce798b043660bad6152a3e2
CodeByJeo <georgefl.business@gmail.com>
2026-05-06 22:02:35 +0300

    kanw ena aplo test

--- src/gr/aueb/cf/schoolapp/Main.java
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -3,7 +3,9 @@ package gr.aueb.cf.schoolapp;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello kateteti");
+        String[] test = {"test","test2"};
+        System.out.println(test[0]);
+        System.out.println("test");
     }
 
 }
