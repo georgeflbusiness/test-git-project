9ae79c279b6be24058784845fb7bd83ffa17aa59
CodeByJeo <georgefl.business@gmail.com>
2026-05-06 22:37:03 +0300

    test den kserw posa exw kanei

--- src/gr/aueb/cf/schoolapp/Main.java
+++ src/gr/aueb/cf/schoolapp/Main.java
@@ -3,9 +3,20 @@ package gr.aueb.cf.schoolapp;
 public class Main {
 
     public static void main(String[] args) {
-        String[] test = {"test","test2"};
+        String[] test = {"test","test2","τεστ3"};
         System.out.println(test[0]);
         System.out.println("test");
+        printArray(test);
+
     }
+
+    public static void printArray(String[] arr) {
+
+        for (String token : arr) {
+            System.out.println(token);
+        }
+    }
+
+
 
 }
